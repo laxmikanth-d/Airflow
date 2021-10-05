3af2ae38f72cd520b4e0e0e372e0f1267d964d5c
Laxmikanth <32249729+laxmikanth-d@users.noreply.github.com>
2021-10-05 15:41:39 -0700

    Write file DAG

---
+++ a_write_file.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+import os
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'email': 'lax@airflow.com',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1)
+}
+
+
+def file_write_method():
+    with open('airflow_test_write.txt', 'w') as f:
+        # Path here is /home/lax
+        f.write('Laxmikanth weldone')
+        f.close()
+    cwd = os.getcwd()
+    return cwd
+
+
+with DAG(
+    'write_file',
+    default_args=default_args,
+    description='Write file',
+    start_date=days_ago(1),
+    tags=['lax']
+) as dag:
+
+    fw = PythonOperator(
+        task_id='file_write',
+        python_callable=file_write_method
+    )
+
+fw
