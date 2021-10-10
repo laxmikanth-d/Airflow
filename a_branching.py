73bc1901a7062c4208c163451ed0115fe995df16
Laxmikanth <32249729+laxmikanth-d@users.noreply.github.com>
2021-10-10 15:08:54 -0700

    Correcting the OperatorName for branching task.

--- a_branching.py
+++ a_branching.py
@@ -3,8 +3,7 @@ from datetime import datetime, timedelta
 import random
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
 
 
 default_args = {
@@ -33,33 +32,47 @@ def greet_good_bye():
     print('Bye bye.')
 
 
+def hello_1_4():
+    print('In method: hello_1_4')
+
+
+def hello_4_8():
+    print('In method: hello_4_8')
+
+
+def hello_8_10():
+    print('In method: hello_8_10')    
+
 
 with DAG('branching',default_args=default_args, catchup=False, tags=['lax']
 , schedule_interval='@once') as dag:
 
-    decider = PythonOperator(
+    decider = BranchPythonOperator(
         task_id = 'decider',
-        python_callable=decide_branch
+        python_callable=decide_branch,
+        do_xcom_push = False
     )
 
-    hello_1_4 = DummyOperator(
-        task_id = 'hello_1_4'
+    hello_1_4 = PythonOperator(
+        task_id = 'hello_1_4',
+        python_callable=hello_1_4
     )
 
-    hello_4_8 = DummyOperator(
-        task_id = 'hello_4_8'
+    hello_4_8 = PythonOperator(
+        task_id = 'hello_4_8',
+        python_callable=hello_4_8
     )
 
-    hello_8_10 = DummyOperator(
-        task_id = 'hello_8_10'
+    hello_8_10 = PythonOperator(
+        task_id = 'hello_8_10',
+        python_callable=hello_8_10
     )
 
     good_bye = PythonOperator(
         task_id = 'good_bye',
         python_callable=greet_good_bye,
-        trigger_rule='none_failed_or_skipped'
+        trigger_rule='one_success'
     )
-
 
 
     decider >> [hello_1_4, hello_4_8, hello_8_10] >> good_bye
