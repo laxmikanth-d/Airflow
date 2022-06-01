ec6eaba95484819804e88ad471ae1f0969a5a696
Laxmikanth <32249729+laxmikanth-d@users.noreply.github.com>
2022-05-31 22:34:47 -0700

    Simplified s3 sensor

--- a_s3key_sensor.py
+++ a_s3key_sensor.py
@@ -4,23 +4,21 @@ from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
 
 default_args = {
     'owner': 'Lax',
-    'start_date': datetime(2021,12,10),
+    'start_date': datetime(2022, 5, 18),
     'email_on_failure': False,
     'email_on_retry': False,
     'depends_on_past': False,
-    'retries': 0,
+    'retries': 0
 }
 
 with DAG('s3key_sensor', default_args=default_args, schedule_interval="@once", tags=['lax']) as dag:
 
-    t1 = S3KeySensor(
-        task_id='s3key_sensor',
-        bucket_name='jsonfiles10052021',
-        bucket_key='*.json',
-        wildcard_match=True,
-        aws_conn_id='AWSS3Operations'
-    )
+   t1 = S3KeySensor(
+       task_id='s3key_sensor',
+       bucket_name='input-files-json',
+       bucket_key="*.txt",
+       wildcard_match=True,
+       aws_conn_id='aws_default'
+   ) 
 
-
-    t1
-    
+   t1
